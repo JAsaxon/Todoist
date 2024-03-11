7c37f2b6ce583e221012f4f754703a2700eecc39
Jorge <jorgesaxon1234@gmail.com>
2024-03-10 18:42:37 -0600

    handle input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react"
-import "./App.scss"
-import { task } from "./types.ts"
+import { FormEvent, useEffect, useRef, useState } from "react";
+import "./App.scss";
+import { task } from "./types.ts";
+import { Value } from "sass";
 function App() {
-  const [Tasks, setTasks] = useState<task[]>([])
-  function handleAdd(task: task) {}
+  const [Tasks, setTasks] = useState<task[]>([]);
+  function handleAdd(task: task) {
+    setTasks((prev) => [...prev, task]);
+  }
   return (
     <>
       <div className="app-box">
@@ -25,29 +28,30 @@ function App() {
         <div className="main-app">
           <h1 className="section">Today</h1>
           <Add handleAdd={handleAdd} />
+          <TaskList tasks={Tasks} />
         </div>
       </div>
     </>
-  )
+  );
 }
 export type addProps = {
-  handleAdd: (Task: task) => void
-}
+  handleAdd: (Task: task) => void;
+};
 type taskListProps = {
-  tasks: task[]
-}
+  tasks: task[];
+};
 function TaskList({ tasks }: taskListProps) {
   return (
     <div className="taskList">
       {tasks.map((task: task) => {
-        return <Task task={task} />
+        return <Task task={task} />;
       })}
     </div>
-  )
+  );
 }
 type taskJsxProps = {
-  task: task
-}
+  task: task;
+};
 function Task({ task }: taskJsxProps) {
   return (
     <div className="task">
@@ -60,15 +64,65 @@ function Task({ task }: taskJsxProps) {
       <h1>{task.Title}</h1>
       <p>{task.Description}</p>
     </div>
-  )
+  );
 }
 const Add = ({ handleAdd }: addProps) => {
+  const [formOpen, setFormOpen] = useState(false);
+  function handleClose() {
+    console.log("y58r", formOpen);
+    setFormOpen(false);
+  }
   return (
     <div className="add-section">
-      <span className="add">+</span>
-      <span className="text">Add task...</span>
+      {!formOpen && (
+        <div onClick={() => setFormOpen(true)}>
+          {" "}
+          <span className="add">+</span>
+          <input type="text" className="text" placeholder="Add task..." />
+        </div>
+      )}
+      {formOpen && <AddForm handleAdd={handleAdd} handleClose={handleClose} />}
     </div>
-  )
+  );
+};
+type addFormProps = addProps & {
+  handleClose: () => void;
+};
+function AddForm({ handleAdd, handleClose }: addFormProps) {
+  const name = useRef<HTMLInputElement>(null);
+  const description = useRef<HTMLInputElement>(null);
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log(name.current!.value, description.current!.value);
+    handleAdd({
+      Title: name.current!.value,
+      Description: description.current!.value,
+      Section: "TODAY",
+      Completed: false,
+    });
+    handleClose();
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name of the task"
+        name="name"
+        ref={name}
+      />
+      <input
+        type="text"
+        placeholder="Description"
+        className="description"
+        ref={description}
+      />
+      <div className="buttons">
+        <button onClick={() => handleClose()} type="button">
+          Close
+        </button>
+        <button>Add Task</button>
+      </div>
+    </form>
+  );
 }
-
-export default App
+export default App;
